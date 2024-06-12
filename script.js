03db592647c042e3961fc843bfa8370c8637c8b6
OmprakashMehta <Omprakashverma090909@gmail.com>
2024-06-12 14:33:17 +0530

    new update

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ let date = new Date();
 let year = date.getFullYear();
 
 document.getElementById("year").innerText = year;
+
+// Show or hide the Go to Top button based on scroll position
+window.onscroll = function () {
+  let goToTopButton = document.getElementById("goToTop");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goToTopButton.classList.add("active");
+  } else {
+    goToTopButton.classList.remove("active");
+  }
+};
+
 // let count = 0;
 // async function test(){
 //    await setTimeout(() => {
